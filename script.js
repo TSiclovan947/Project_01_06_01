8741931e79f71d922e9a17f918be4b6a59fcd56c
Unknown <tsiclovan947@west-mec.org>
2018-08-15 18:26:10 -0700

    SetUp Project
    Added author name and date; Created a javascript file

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+      script.js
+      Form Validation Functions for index.html
+      
+      Author: Tabitha Siclovan
+      Date: 15 August 2018
+      
+*/
+
+"use strict";
+
+var formValidity = true;
+
